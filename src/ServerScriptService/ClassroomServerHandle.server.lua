54c951666908bb28c9462e087196a7b3bd4c4203
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-04-17 18:31:54 -0400

    Change class every 4 hours

--- src/ServerScriptService/ClassroomServerHandle.server.lua
+++ src/ServerScriptService/ClassroomServerHandle.server.lua
@@ -1,8 +1,18 @@
 local classroomService = require(game.ReplicatedStorage.PublicDependancies.Classroom)
+local logTime
 
 game.Players.PlayersAdded:Connect(function(player)
-    if player then
-        local lastLogTime = player.Leaderstats.lastLogTime
-        
-    end
+	if player then
+		local lastLogTime = player.Leaderstats.lastLogTime
+		logTime = lastLogTime
+		if os.time() >= lastLogTime.Value + 14400 then
+			classroomService:ChangeClass()
+		end
+		while wait() do
+			if os.time() >= logTime then
+				logTime = os.time()
+				classroomService:ChangeClass()
+			end
+		end
+	end
 end)
