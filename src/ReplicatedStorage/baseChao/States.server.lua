1730866e421d16993291e19d044922a36adf22f5
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-04-17 00:38:40 -0400

    Implement Swim and Fly anims

--- src/ReplicatedStorage/baseChao/States.server.lua
+++ src/ReplicatedStorage/baseChao/States.server.lua
@@ -1,7 +1,6 @@
 --[[
     States
     Handles animating the chao
-
 ]]
 
 --Collect the chao and the HumanoidObject
@@ -9,6 +8,8 @@ local chao = script.Parent
 local hum = script.Parent.Humanoid
 --Get the runtime.
 local RS = game:GetService("RunService")
+--ChaoState
+local ChaoState = "none"
 
 --Runs on every frame.
 RS.Heartbeat:Connect(function()
@@ -19,8 +20,33 @@ RS.Heartbeat:Connect(function()
     local humState = hum:GetState()
     --Check the humState
     if humState == Enum.HumanoidStateType.Running then
-        if currentlyPlaying ~= "Running" then
-            currentlyPlaying =  "Running"
+        if ChaoState == "Swimming" then
+            if currentlyPlaying ~= "Swimming" then
+                currentlyPlaying =  "Swimming"
+                if anim.IsPlaying == true then
+                    anim:Stop()
+                end
+                --TODO: Show different anims depending on chaos skill
+                anim.Id = "rbxassetid://" --Make anim
+                hum.Animator:LoadAnimation(anim)
+                anim:Play()
+            end
+        elseif ChaoState == "none" then
+            if currentlyPlaying ~= "Running" then
+                currentlyPlaying =  "Running"
+                if anim.IsPlaying == true then
+                    anim:Stop()
+                end
+                --TODO: Show different anims depending on chaos skill
+                anim.Id = "rbxassetid://" --Make anim
+                hum.Animator:LoadAnimation(anim)
+                anim:Play()
+            end
+        end
+    elseif humState == Enum.HumanoidStateType.Freefall then
+        if currentlyPlaying ~= "Flying" then
+            --TODO: Check if they can actually fly
+            currentlyPlaying = "Flying"
             if anim.IsPlaying == true then
                 anim:Stop()
             end
@@ -38,5 +64,14 @@ RS.Heartbeat:Connect(function()
             hum.Animator:LoadAnimation(anim)
             anim:Play()
         end
+    end
+end)
+
+--Change the chao's state to swimming if they're touching a part named "Water"
+script.Parent.Touched:Connect(function(hit)
+    if hit.Name == "Water" then
+        ChaoState = "Swimming"
+    else
+        ChaoState = "none"
     end
 end)
