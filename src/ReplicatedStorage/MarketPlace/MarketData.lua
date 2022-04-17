d9ba649c22fc207eda5afebde5a04bf93d93e735
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-04-16 23:57:30 -0400

    Add Chao Fruits to MarketData

--- src/ReplicatedStorage/MarketPlace/MarketData.lua
+++ src/ReplicatedStorage/MarketPlace/MarketData.lua
@@ -10,6 +10,62 @@ market = {
 		Price = "100",
 		forSale = true,
 		desc = "A tasty fruit all chao know and love,",
+	},
+	SFruit = {
+		Name = "Square Fruit",
+		desc = "A square shaped fruit. Some chao love this.",
+		isDecor = false,
+		Price = "100",
+		forSale = true
+	},
+	RFruit = {
+		Name = "Round Fruit",
+		desc = "A round shaped fruit. Some chao love this.",
+		isDecor = false,
+		Price = "125",
+		forSale = true
+	},
+	TFruit = {
+		Name = "Triangle Fruit",
+		desc = "A triangle shaped fruit. Some chao love this.",
+		isDecor = false,
+		Price = "150",
+		forSale = true
+	},
+	StFruit = {
+		Name = "Strong Fruit",
+		desc = "Help your chao grow stronger. (Power +4)",
+		isDecor = false,
+		Price = "200",
+		forSale = false
+	},
+	TaFruit = {
+		Name = "Tasty Fruit",
+		desc = "A tasty fruit that all chao love. Its great for their health too.",
+		isDecor = false,
+		Price = "350",
+		forSale = false
+	},
+	HFruit = {
+		Name = "Heart Fruit",
+		desc = "A sweet tasting fruit that gets chao ready to mate.",
+		isDecor = false,
+		Price = "500",
+		forSale = false
+	},
+	SMFruit = {
+		Name = "Smart Fruit",
+		desc = "A fruit with riveting flavor that helps your chao learn.",
+		isDecor = false,
+		Price = "500",
+		forSale = false
+	},
+	Mint = {
+		Name = "Mint Candy",
+		desc = "A lucky piece of candy sure to give your chao tons of energy. (Luck +25)",
+		isDecor = false,
+		Price = "800"
+		forSale = false
 	}
 }
 
