c0eeef8c1de37e5a6db7ebefb64b4299d3722a16
Silver <kylekates.gamin@gmail.com>
2022-04-20 20:13:22 -0400

    Complete MainOptionsHandle

--- src/StarterGui/Fortune_Teller/Frame/Options/MainOptionsHandle.client.lua
+++ src/StarterGui/Fortune_Teller/Frame/Options/MainOptionsHandle.client.lua
@@ -14,6 +14,11 @@ ourEvent.Event:Connect(function()
 					elseif v.Name == "Solo" then
 						finEvent:Fire()
 						script.Parent.Visible = false
+						--Shows the make a name UI
+						script.MakeAName.Visible = true
+					elseif v.Name == "Enter" then
+						game.ReplicatedStorage.Remotes.FinishName:FireServer(v.Parent.Text,game.Players.LocalPlayer.Character:FindFirstChild("Held",true).Parent,game.Players.LocalPlayer)
+						print("Finalizing Name")
 					end
 				end)
 			end
