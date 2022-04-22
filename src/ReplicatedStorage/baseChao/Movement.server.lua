f10916b36b70021e1fe1329905db645ec7d288f7
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-04-22 17:58:33 -0400

    Mark Movement for deprication

--- src/ReplicatedStorage/baseChao/Movement.server.lua
+++ src/ReplicatedStorage/baseChao/Movement.server.lua
@@ -1,77 +1,79 @@
-math.randomseed(tick())
---Services
-local PFS = game:GetService("PathfindingService")
-local path = PFS:CreatePath()
-local pathinprogress = false
+--Depricate this. States handles movement and animations
 
---update wait()
-local wait = task.wait
+-- math.randomseed(tick())
+-- --Services
+-- local PFS = game:GetService("PathfindingService")
+-- local path = PFS:CreatePath()
+-- local pathinprogress = false
 
---TODO: Add states that the chao can use to show the correct animation.
---[[
-	States
-	Walk --> Chao is walking around.
-	Normal --> The Idle State.
-	Thinking --> The State that comes right before the walk state. The chao is deciding what to do.
-	Sleeping --> The chao is recharging it energy.
-]]
+-- --update wait()
+-- local wait = task.wait
 
-function followPath(object, goal)
-	local waypoints
-	local hum = object.Parent.Humanoid
-	local success, failed = pcall(function()
-		path:ComputeAsync(object.Position, goal)
-	end)
+-- --TODO: Add states that the chao can use to show the correct animation.
+-- --[[
+-- 	States
+-- 	Walk --> Chao is walking around.
+-- 	Normal --> The Idle State.
+-- 	Thinking --> The State that comes right before the walk state. The chao is deciding what to do.
+-- 	Sleeping --> The chao is recharging it energy.
+-- ]]
 
-	if success and path.Status == Enum.PathStatus.Success then
-		waypoints = path:GetWaypoints()
-		for i, waypoint in pairs(waypoints) do
-			if script.Parent.Held.Value == false then
-				if waypoint.Action == Enum.PathWaypointAction.Jump then
-					wait(1) --make the chao pause before jumping
-					hum:ChangeState(Enum.HumanoidStateType.Jumping)
-				end
-				hum:MoveTo(waypoint.Position)
-				hum.MoveToFinished:Wait(1)
-			else
-				break
-			end
-		end
-		pathinprogress = false
-	else
-		warn("Unable to compute path. "..tostring(failed))
-	end
-end
+-- function followPath(object, goal)
+-- 	local waypoints
+-- 	local hum = object.Parent.Humanoid
+-- 	local success, failed = pcall(function()
+-- 		path:ComputeAsync(object.Position, goal)
+-- 	end)
 
-local function generateMovement()
-	local startPos = script.Parent.HumanoidRootPart.Position
-	local walls = workspace.Walls
-	local outsideForce = false
-	local randomVector
-	for i, v in pairs(walls:GetChildren()) do
-		local magnitude = (startPos.Magnitude - v.Position.Magnitude)
-		if magnitude <= 100 then
-			--return them to the center
-			outsideForce = true
-			randomVector = Vector3.new(0,0,0) --where the origin will be
-		end
-	end
-	if outsideForce == false then
-		--Move around 100 studs
-		randomVector = Vector3.new(startPos.X+math.random(-100,100),startPos.Y,startPos.Z+math.random(-100,100)) --add
-	end
-	followPath(script.Parent.HumanoidRootPart, randomVector)
-end
+-- 	if success and path.Status == Enum.PathStatus.Success then
+-- 		waypoints = path:GetWaypoints()
+-- 		for i, waypoint in pairs(waypoints) do
+-- 			if script.Parent.Held.Value == false then
+-- 				if waypoint.Action == Enum.PathWaypointAction.Jump then
+-- 					wait(1) --make the chao pause before jumping
+-- 					hum:ChangeState(Enum.HumanoidStateType.Jumping)
+-- 				end
+-- 				hum:MoveTo(waypoint.Position)
+-- 				hum.MoveToFinished:Wait(1)
+-- 			else
+-- 				break
+-- 			end
+-- 		end
+-- 		pathinprogress = false
+-- 	else
+-- 		warn("Unable to compute path. "..tostring(failed))
+-- 	end
+-- end
+
+-- local function generateMovement()
+-- 	local startPos = script.Parent.HumanoidRootPart.Position
+-- 	local walls = workspace.Walls
+-- 	local outsideForce = false
+-- 	local randomVector
+-- 	for i, v in pairs(walls:GetChildren()) do
+-- 		local magnitude = (startPos.Magnitude - v.Position.Magnitude)
+-- 		if magnitude <= 100 then
+-- 			--return them to the center
+-- 			outsideForce = true
+-- 			randomVector = Vector3.new(0,0,0) --where the origin will be
+-- 		end
+-- 	end
+-- 	if outsideForce == false then
+-- 		--Move around 100 studs
+-- 		randomVector = Vector3.new(startPos.X+math.random(-100,100),startPos.Y,startPos.Z+math.random(-100,100)) --add
+-- 	end
+-- 	followPath(script.Parent.HumanoidRootPart, randomVector)
+-- end
 
 
-wait(5)
+-- wait(5)
 
-while wait(1) do
-	if script.Parent.Parent == workspace then
-		if script.Parent.Held.Value == false then
-			if pathinprogress == false then
-				generateMovement()
-			end
-		end
-	end
-end
+-- while wait(1) do
+-- 	if script.Parent.Parent == workspace then
+-- 		if script.Parent.Held.Value == false then
+-- 			if pathinprogress == false then
+-- 				generateMovement()
+-- 			end
+-- 		end
+-- 	end
+-- end
