ef91f2641e229f67bb0afae11f293e82c2119566
Silver <kylekates.gamin@gmail.com>
2022-04-23 09:54:32 -0400

    Clean up AwaitHandles

--- src/StarterGui/Fortune_Teller/Frame/Await/HandleAwait.client.lua
+++ src/StarterGui/Fortune_Teller/Frame/Await/HandleAwait.client.lua
@@ -1,22 +1,6 @@
 local awaitEvent = game.Players.LocalPlayer.PlayerGui.AwaitApproval
 local nextEvent = game.Players.LocalPlayer.PlayerGui.ApprovalSent
 
---ourEvent.Event:Connect(function()
---	script.Parent.Visible = true
---	while wait() do
---		for i,v in pairs(script.Parent:GetChildren()) do
---			if v:IsA("TextButton") then
---				v.MouseButton1Click:Connect(function()
---					if v.Name == "Generate" then
---						nextEvent:Fire()
---						script.Parent.Visible = false
---					end
---				end)
---			end
---		end
---	end
---end)
-
 --Connect to the request. We'll need to recieve the quest here since we're gonna send it back
 awaitEvent.Event:Connect(function(req,quest)
 	if req == "ChaoName" then
