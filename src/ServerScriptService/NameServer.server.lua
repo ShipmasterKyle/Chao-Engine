374bf192d771a932a15c9b64744c7ff9ed86332d
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-05-04 09:21:28 -0400

    Fix autogenerated names not saving
    I think I never finished writing this until now. :yikes:

--- src/ServerScriptService/NameServer.server.lua
+++ src/ServerScriptService/NameServer.server.lua
@@ -1,9 +1,16 @@
 local event = game.ReplicatedStorage.Remotes.FinishName
+local nameve = game.ReplicatedStorage.Remotes.NameChao
 local namespace = require(game.ReplicatedStorage.PublicDependancies.NameSpaceService)
 
---Just pickup the event since we're already
+--Just pickup the event
 event.OnServerEvent:Connect(function(name,chao,player)
     print("Request Caught")
+	namespace:FinalizeName(name,chao,player)
+	event:FireClient(name)
+end)
+
+nameve.OnServerEvent:Connect(function()
+	print("Request Caught")
 	namespace:FinalizeName(name,chao,player)
 	event:FireClient(name)
 end)
